783894d680a3e1000ee3ca0f3246e454b6e613f5
Mitchell Irey <144982867+mitchellirey@users.noreply.github.com>
2024-12-29 10:54:43 -0500

    test

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # EssentialMode-V2
- 
+Kanersps EssentialMode V2 with SQL
+
+# installation
+* Change credentials in ***essentialmode/server/player/login.lua*** and ***[essential]/es_admin/sv_admin.lua***
+* Import SQL to database
+* Add *- es_admin* and *- essentialmode* to your AutoStartResources in *citmp-server.yml*
+* Find **fivem-map-skater** in your */resources* 
+* Open **__resource.lua** and change
+```
+resource_type 'map' { gameTypes = { fivem = true } }
+```
+to
+```
+resource_type 'map' { gameTypes = { EssentialMode = true } }
+```
+
+# I will not help install or help fix anything you can't get working. I'm simply providing files for you.
+
+Credits to Kanersps
---
+++ essential_mode_v2/sql.sql
@@ -0,0 +1,48 @@
+-- --------------------------------------------------------
+-- Host:                         127.0.0.1
+-- Server versie:                10.1.21-MariaDB - mariadb.org binary distribution
+-- Server OS:                    Win64
+-- HeidiSQL Versie:              9.3.0.4984
+-- --------------------------------------------------------
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8mb4 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+
+-- Databasestructuur van gta5_gamemode_essential wordt geschreven
+CREATE DATABASE IF NOT EXISTS `gta5_gamemode_essential` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `gta5_gamemode_essential`;
+
+
+
+-- Structuur van  tabel gta5_gamemode_essential.bans wordt geschreven
+CREATE TABLE IF NOT EXISTS `bans` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `banned` varchar(50) NOT NULL DEFAULT '0',
+  `banner` varchar(50) NOT NULL,
+  `reason` varchar(150) NOT NULL DEFAULT '0',
+  `expires` datetime NOT NULL,
+  `timestamp` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- Data exporteren was gedeselecteerd
+
+
+-- Structuur van  tabel gta5_gamemode_essential.users wordt geschreven
+CREATE TABLE IF NOT EXISTS `users` (
+  `identifier` varchar(255) CHARACTER SET utf8mb4 NOT NULL DEFAULT '',
+  `permission_level` int(255) NOT NULL DEFAULT '0',
+  `group` varchar(50) NOT NULL DEFAULT '0',
+  `money` double NOT NULL DEFAULT '0',
+  `weapons` varchar(5000) CHARACTER SET utf8 NOT NULL,
+  `personalvehicle` varchar(10000) CHARACTER SET utf8 NOT NULL,
+  `withdraw_cost` int(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+-- Data exporteren was gedeselecteerd
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
